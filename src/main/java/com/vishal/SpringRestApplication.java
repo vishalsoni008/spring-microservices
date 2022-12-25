bb480fa09a6a545cc8e3b26fbc70f6e32730c59c
Vishal Soni <50075856+vishalsoni008@users.noreply.github.com>
2022-12-25 14:15:11 +0530

    a

--- src/main/java/com/vishal/SpringRestApplication.java
+++ src/main/java/com/vishal/SpringRestApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringRestApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApplication.class, args);
 	}
